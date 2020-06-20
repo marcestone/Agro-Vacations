8bb49dcd6aaee97b51d09d14ce282c775e7bb7bd
AndresFelipeCastellanos <afcastellanosn@unal.edu.co>
2020-06-19 19:13:08 -0500

    Testing crear y filtro

--- tests/unit/testingCrearAct.spec.js
+++ tests/unit/testingCrearAct.spec.js
@@ -17,6 +17,47 @@ describe("registro" , () => {
         expect(wrapper.find("button").exists()).toBe(true)
     })
 
+    it("Nombre actividad" ,() => {
+        wrapper.find("#inputName").setValue("Paseo"); 
+        const submit = wrapper.find("button");
+        submit.trigger('click');
+        expect(wrapper.vm.$data.activityName).toBe("Paseo");
+    }) 
+
+    it("Precio actividad" ,() => {
+        wrapper.find("#inputName").setValue("Paseo"); 
+        const submit = wrapper.find("button");
+        submit.trigger('click');
+        wrapper.find("#inputPrice").setValue("100000"); 
+    })  
+
+    it("Nombre actividad" ,() => {
+        wrapper.find("#inputType").setValue("Extrema");  
+        const submit = wrapper.find("button");
+        submit.trigger('click');
+        expect(wrapper.vm.$data.activityType).toBe("Extrema");
+    })  
+
+    it("Nombre actividad" ,() => {
+        wrapper.find("#inputLocation").setValue("Bogota");
+        const submit = wrapper.find("button");
+        submit.trigger('click');
+        expect(wrapper.vm.$data.activityLocation).toBe("Bogota");
+    })  
+
+    it("Nombre actividad" ,() => {
+        wrapper.find("#inputTransport").findAll("option").at(0).setSelected(); 
+        const submit = wrapper.find("button");
+        submit.trigger('click');
+        expect(wrapper.vm.$data.activityTransport).toBe("Yes");
+    })  
+    it("Nombre actividad" ,() => {
+        wrapper.find("#description").setValue("Descripción de prueba");
+        const submit = wrapper.find("button");
+        submit.trigger('click');
+        expect(wrapper.vm.$data.description).toBe("Descripción de prueba");
+    })  
+
     it("Datos coincidentes", () =>{    
 
         //Datos de prueba
@@ -29,20 +70,20 @@ describe("registro" , () => {
         wrapper.find("#description").setValue("Descripción de prueba");
         //Actualizar variables con entradas 
 
-        const submit = wrapper.find("button")
-        submit.trigger('click')  
+        const submit = wrapper.find("button");
+        submit.trigger('click'); 
 
         //await submit.trigger("click")
 
 
         //Llamar a la función passTwo de watch.
 
-        expect(wrapper.vm.$data.activityName).toBe("Paseo")
-        expect(wrapper.vm.$data.activityPrice).toBe("100000")
-        expect(wrapper.vm.$data.activityType).toBe("Extrema")
-        expect(wrapper.vm.$data.activityLocation).toBe("Bogota")
-        expect(wrapper.vm.$data.activityTransport).toBe("Yes")
-        expect(wrapper.vm.$data.description).toBe("Descripción de prueba")
+        expect(wrapper.vm.$data.activityName).toBe("Paseo");
+        expect(wrapper.vm.$data.activityPrice).toBe("100000");
+        expect(wrapper.vm.$data.activityType).toBe("Extrema");
+        expect(wrapper.vm.$data.activityLocation).toBe("Bogota");
+        expect(wrapper.vm.$data.activityTransport).toBe("Yes");
+        expect(wrapper.vm.$data.description).toBe("Descripción de prueba");
 
     })
 })
